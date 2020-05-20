efbc93fd3b50775e301f38a6bba7ee547bcc59fe
Abc <Abc@DESKTOP-EBI5UK2>
2020-05-20 19:57:25 +0530

    Commit : First commit on branch 1

--- JenkinsTest/src/main/java/JenkinsTest/C1.java
+++ JenkinsTest/src/main/java/JenkinsTest/C1.java
@@ -5,4 +5,9 @@ public class C1 {
 	public static void main(String[] args) {
 		System.out.println("This is First class");
 	}
+	
+	public static void Method2()
+	{
+		System.out.println("This is 2nd method ");
+	}
 }
